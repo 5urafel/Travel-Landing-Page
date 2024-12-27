e55407565f6569dbe188b5a8de53de799848cfda
5urafel <surafel5540f@gmail.com>
2024-12-27 08:01:26 +0000

    folder setups , constants, tailwind helper classes, assets

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UIUX next app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Navbar />
+      <main className="relative overflow-hidden">{children}</main>
+      <Footer />
+    </html>
+  );
+}
